95c2b5dde8699326c630158fb0971ecf5fe457ee
Shivani Hanji <quic_shanji@quicinc.com>
2022-09-06 20:31:39 +0530

    msm: eva: Enable SynxV2 for EVA on anorak
    EVA fence APIs need SynxV2 on anorak
    
    Change-Id: I82411a7fd53b0bff767a9e84c6d75b86c3505cb7
    Signed-off-by: Shivani Hanji <quic_shanji@quicinc.com>

--- msm/eva/cvp_hfi.c
+++ msm/eva/cvp_hfi.c
@@ -84,10 +84,8 @@ static bool __is_session_valid(struct iris_hfi_device *device,
 static int __iface_cmdq_write(struct iris_hfi_device *device,
 					void *pkt);
 static int __load_fw(struct iris_hfi_device *device);
-#ifndef HALLIDAY_DISABLE
 static int __dev_regspace_mapping(struct iris_hfi_device *device);
 static int __dev_regspace_unmap(struct iris_hfi_device *device);
-#endif
 static void __unload_fw(struct iris_hfi_device *device);
 static int __tzbsp_set_cvp_state(enum tzbsp_subsys_state state);
 static int __enable_subcaches(struct iris_hfi_device *device);
@@ -1916,13 +1914,11 @@ static int iris_hfi_core_init(void *device)
 	dev->bus_vote.data_count = 1;
 	dev->bus_vote.data->power_mode = CVP_POWER_TURBO;
 
-	#ifndef HALLIDAY_DISABLE
     rc = __dev_regspace_mapping(dev);
     if (rc) {
         dprintk(CVP_ERR, "Failed to do Devices RegisterSpace Mapping FW\n");
       //  goto err_load_fw;
     }
-    #endif
 
 	rc = __load_fw(dev);
 	if (rc) {
@@ -2094,7 +2090,7 @@ static int iris_hfi_core_release(void *dev)
 
 	__disable_subcaches(device);
 	__unload_fw(device);
-
+		__dev_regspace_unmap(device);
 	if (msm_cvp_mmrm_enabled) {
 		rc = msm_cvp_mmrm_deregister(device);
 		if (rc) {
@@ -4435,13 +4431,12 @@ err_iris_power_on:
 	return rc;
 }
 
-#ifndef HALLIDAY_DISABLE
 static int __dev_regspace_mapping(struct iris_hfi_device *device)
 {
     int rc = 0;
     struct context_bank_info *cb;
-    struct subcache_info *sinfo = NULL;
-    uint32_t scid = 0;
+    // struct subcache_info *sinfo = NULL;
+    // uint32_t scid = 0;
     //non-secure context bank
     cb = msm_cvp_smem_get_context_bank(device->res, 0);
         if (!cb) {
@@ -4573,7 +4568,7 @@ static int __dev_regspace_mapping(struct iris_hfi_device *device)
     }
     dprintk(CVP_INFO,"sssanjee iommu_map3\n");
     return rc;
-err_subcache_get:
+// err_subcache_get:
 	__deinit_subcaches(device);
 	return rc;
 }
@@ -4586,7 +4581,9 @@ static int __dev_regspace_unmap(struct iris_hfi_device *device)
     //non-secure context bank
     cb = msm_cvp_smem_get_context_bank(device->res, 0);
     if (!cb) {
-            dprintk(CVP_ERR," %s: failed to get context bank\n", __func__);
+        dprintk(CVP_ERR," %s: failed to get context bank\n", __func__);
+        rc = -EINVAL;
+        goto err_get_context_bank;
     }
     iommu_unmap(cb->domain, device->res->ipclite_iova, device->res->ipclite_size);//
 
@@ -4596,10 +4593,10 @@ static int __dev_regspace_unmap(struct iris_hfi_device *device)
 //    iommu_unmap(cb->domain, device->res->display_iova, device->res->display_size);//
 //    iommu_unmap(cb->domain, device->res->aontimers_iova, device->res->aontimers_size);//
     iommu_unmap(cb->domain, device->res->hwmutex_iova, device->res->hwmutex_size);//
-    return rc;
 
+err_get_context_bank:
+    return rc;
 }
-#endif
 
 static int __load_fw(struct iris_hfi_device *device)
 {
