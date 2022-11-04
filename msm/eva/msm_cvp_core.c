3241b65d2fd949a2aec467036e621062ba160e18
Aniruddh Sharma <quic_anirshar@quicinc.com>
2022-11-04 13:17:27 +0530

    msm: eva: Avoid mutex lock while holding spin lock
    spin_lock disables preemption. If we try to acquire the mutex while
    holding the spin lock and if it get's blocked due to mutex was already
    acquired by other thread, then this will lead the current thread to go
    into blocked state but CPU can't schedule another thread as preemption
    is disabled by spin lock. This is a deadlock now.
    Mutex lock is now not allowed inside the spin lock.
    
    Change-Id: I9c9c4a4fda2564890743381af923614b54158a55
    Signed-off-by: Aniruddh Sharma <quic_anirshar@quicinc.com>

--- msm/eva/msm_cvp_core.c
+++ msm/eva/msm_cvp_core.c
@@ -256,8 +256,9 @@ static void msm_cvp_clean_sess_queue(struct msm_cvp_inst *inst,
 		struct cvp_session_queue *sq)
 {
 	struct cvp_session_msg *mptr, *dummy;
-	u64 ktid;
+	u64 ktid  = 0LL;
 
+check_again:
 	spin_lock(&sq->lock);
 	if (sq->msg_count && sq->state != QUEUE_ACTIVE) {
 		list_for_each_entry_safe(mptr, dummy, &sq->msgs, node) {
@@ -265,13 +266,21 @@ static void msm_cvp_clean_sess_queue(struct msm_cvp_inst *inst,
 			if (ktid) {
 				list_del_init(&mptr->node);
 				sq->msg_count--;
-				msm_cvp_unmap_frame(inst, ktid);
-				kmem_cache_free(cvp_driver->msg_cache, mptr);
+				break;
 			}
 		}
 	}
 	spin_unlock(&sq->lock);
+
+	if (ktid) {
+		msm_cvp_unmap_frame(inst, ktid);
+                kmem_cache_free(cvp_driver->msg_cache, mptr);
+		mptr = NULL;
+		ktid = 0LL;
+		goto check_again;
+	}
 }
+
 static int msm_cvp_session_stop_notify(struct msm_cvp_inst *inst)
 {
 	int rc = 0;
