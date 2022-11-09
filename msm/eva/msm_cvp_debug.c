3d48b4ce765f75d6c08f37e2ee1981c761fc3f16
Pranay Varma Kopanati <quic_pkopanat@quicinc.com>
2022-11-09 22:57:15 +0530

    msm: eva: Default enablement of minidump for EVA
    Change-Id: Ic8676a48420416a155a7f0023d03223d50d6ec3b
    Signed-off-by: Pranay Varma Kopanati <quic_pkopanat@quicinc.com>

--- msm/eva/msm_cvp_debug.c
+++ msm/eva/msm_cvp_debug.c
@@ -35,7 +35,7 @@ bool msm_cvp_mmrm_enabled = true;
 bool msm_cvp_mmrm_enabled = !true;
 #endif
 bool msm_cvp_dcvs_disable = !true;
-int msm_cvp_minidump_enable = !1;
+int msm_cvp_minidump_enable = 1;
 
 #define MAX_DBG_BUF_SIZE 4096
 
