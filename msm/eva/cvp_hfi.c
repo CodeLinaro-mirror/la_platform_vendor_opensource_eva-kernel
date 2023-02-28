8169000f5c0e4ab52609d24cf20f32d8a1921754
Nagesh Gunna <quic_ngunna@quicinc.com>
2023-02-28 20:45:15 +0530

    eva-kernel: FW debug mask bitwise or with 0x400000
               Currently FW debug mask it or ed with 0x00400000
               to enable timeout handling from FW end by default
              irrespective of debug mask from external cmd.
    
    Change-Id: I88d5f6d3cd57671bd7902a8b2128ca6f1b8de774
    Signed-off-by: Nagesh Gunna <quic_ngunna@quicinc.com>

--- msm/eva/cvp_hfi.c
+++ msm/eva/cvp_hfi.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <asm/memory.h>
@@ -1828,7 +1828,7 @@ static int __sys_set_debug(struct iris_hfi_device *device, u32 debug)
 	int rc = 0;
 	struct cvp_hfi_cmd_sys_set_property_packet *pkt =
 		(struct cvp_hfi_cmd_sys_set_property_packet *) &packet;
-
+        debug = debug | 0x00400000;
 	rc = call_hfi_pkt_op(device, sys_debug_config, pkt, debug);
 	if (rc) {
 		dprintk(CVP_WARN,
