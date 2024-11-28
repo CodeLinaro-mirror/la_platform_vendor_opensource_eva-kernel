eedd43ef061e0da18db9a3222a0def9903e18516
Aniruddh Sharma <quic_anirshar@quicinc.com>
2024-11-28 11:29:31 +0530

    msm: eva: Copy back the validated packet size to avoid security issues
    As we read packets from a shared queue, there is a possibility that a malicious FW corrupts the packet->size data in the shared queue after it has been validated in the kernel driver.
    
    Also fixed KW issue in __process_sys_error (in the newly added SFR buffer size check)
    
    Change-Id: I2f75da29b6abbd3c1826f1b3c9c82ee9678fa60a
    Signed-off-by: Aniruddh Sharma <quic_anirshar@quicinc.com>

--- msm/eva/cvp_hfi.c
+++ msm/eva/cvp_hfi.c
@@ -489,7 +489,7 @@ static int __read_queue(struct cvp_iface_q_info *qinfo, u8 *packet,
 	u32 *read_ptr;
 	u32 receive_request = 0;
 	u32 read_idx, write_idx;
-		int rc = 0;
+	int rc = 0;
 
 	if (!qinfo || !packet || !pb_tx_req_is_set) {
 		dprintk(CVP_ERR, "Invalid Params\n");
@@ -579,6 +579,12 @@ static int __read_queue(struct cvp_iface_q_info *qinfo, u8 *packet,
 					(u8 *)qinfo->q_array.align_virtual_addr,
 					new_read_idx << 2);
 		}
+		/*
+		 * Copy back the validated size to avoid security issues. As we read
+		 * the packet from a shared queue, there is a possibility that the
+		 * packet->size data gets corrupted by malicious firmware.
+		 */
+		*((u32 *) packet) = packet_size_in_words << 2;
 	} else {
 		dprintk(CVP_WARN,
 			"BAD packet received, read_idx: %#x, pkt_size: %d\n",
@@ -2659,22 +2665,26 @@ skip_power_off:
 static void __process_sys_error(struct iris_hfi_device *device)
 {
 	struct cvp_hfi_sfr_struct *vsfr = NULL;
-	u32 sfr_buf_size = 0;
 
 	vsfr = (struct cvp_hfi_sfr_struct *)device->sfr.align_virtual_addr;
-	sfr_buf_size = vsfr->bufSize;
-	if (vsfr && sfr_buf_size < ALIGNED_SFR_SIZE) {
-		void *p = memchr(vsfr->rg_data, '\0', sfr_buf_size);
-		/*
-		 * SFR isn't guaranteed to be NULL terminated
-		 * since SYS_ERROR indicates that Iris is in the
-		 * process of crashing.
-		 */
-		if (p == NULL)
-			vsfr->rg_data[sfr_buf_size - 1] = '\0';
+	if(vsfr) {
+		u32 sfr_buf_size = vsfr->bufSize;
+		if (sfr_buf_size < ALIGNED_SFR_SIZE) {
+			void *p = memchr(vsfr->rg_data, '\0', sfr_buf_size);
+			/*
+			 * SFR isn't guaranteed to be NULL terminated
+			 * since SYS_ERROR indicates that Iris is in the
+			 * process of crashing.
+			 */
+			if (p == NULL)
+				vsfr->rg_data[sfr_buf_size - 1] = '\0';
 
-		dprintk(CVP_ERR, "SFR Message from FW: %s\n",
+			dprintk(CVP_ERR, "SFR Message from FW: %s\n",
 				vsfr->rg_data);
+		}
+	}
+	else {
+		dprintk(CVP_ERR, "Error: vsfr is NULL\n");
 	}
 }
 
