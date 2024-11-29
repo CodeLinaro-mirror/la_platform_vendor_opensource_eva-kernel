2e0031cc035136458bbb4b5c86703d7a5f565a63
Sudheer Gummalla <quic_gummalla@quicinc.com>
2024-11-29 17:42:33 +0530

    eva-kernel: pm qos update regression fix & latency
    at Create sesssion pm_qos_update with latency number from platform data.
    if all Sessions are deleted then vote with default values.
    
    mutex_lock called under spin lock.
    fixed by moving spin_unlock before mutex lock.
    Changes ported from eva-kernel.lnx.1.1 to eva-kernel.lnx.5.0.
    
    Change-Id: Ic176ee615ab8b395f33a774a13eafdc7aaca2fda
    Signed-off-by: Sudheer Gummalla <quic_gummalla@quicinc.com>

--- msm/eva/cvp_hfi_api.h
+++ msm/eva/cvp_hfi_api.h
@@ -265,7 +265,7 @@ struct cvp_hfi_device {
 	int (*flush_debug_queue)(void *dev);
 	int (*noc_error_info)(void *dev);
 	int (*validate_session)(void *sess, const char *func);
-	int (*pm_qos_update)(void *device);
+	int (*pm_qos_update)(void *device, u32 latency);
 };
 
 typedef void (*hfi_cmd_response_callback) (enum hal_command_response cmd,
