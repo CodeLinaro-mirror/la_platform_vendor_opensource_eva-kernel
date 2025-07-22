aa72db29922bf3609423ade4f01d0a24f8e3697b
Pulkit Singh Tak <ptak@qti.qualcomm.com>
2025-07-22 16:43:10 +0530

    msm: eva: Fix for kernel address exposure vulnerability to user
    Driver allocates an object for session structure and then passes
    this address to user after modifing this address a bit using
    hash32_ptr function. This function does not hash the address
    properly and user can retrieve the kernel address back from the
    hashed value.
    
    Change-Id: I7bffba671cf7816a42ec55b8eca711b2394c6e05
    Signed-off-by: Pulkit Singh Tak <ptak@qti.qualcomm.com>

--- msm/eva/cvp_hfi.c
+++ msm/eva/cvp_hfi.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <asm/memory.h>
@@ -41,6 +41,7 @@
 #include "msm_cvp_events.h"
 #include "cvp_comm_def.h"
 #include "cvp_presil.h"
+#include "msm_cvp.h"
 
 // ysi - added for debug
 #include <linux/clk/qcom.h>
@@ -1754,6 +1755,8 @@ static int __interface_dsp_queues_init(struct iris_hfi_device *dev)
 	dma_addr_t dma_handle;
 	dma_addr_t iova;
 	struct context_bank_info *cb;
+	int count = 0;
+	const int max_retries = 10;
 
 	q_size = ALIGN(QUEUE_SIZE, SZ_1M);
 	mem_data = &dev->dsp_iface_q_table.mem_data;
@@ -1763,9 +1766,21 @@ static int __interface_dsp_queues_init(struct iris_hfi_device *dev)
 		cvp_dsp_init_hfi_queue_hdr(dev);
 		return 0;
 	}
-	/* Allocate dsp queues from CDSP device memory */
-	kvaddr = dma_alloc_coherent(dev->res->mem_cdsp.dev, q_size,
+
+	while (count < max_retries) {
+		/* Allocate dsp queues from CDSP device memory */
+		kvaddr = dma_alloc_coherent(dev->res->mem_cdsp.dev, q_size,
 				&dma_handle, GFP_KERNEL);
+		if (IS_ERR_OR_NULL(kvaddr)) {
+			dprintk(CVP_ERR, "%s: failed dma allocation, retry %d\n",
+					__func__, count);
+			usleep_range(100000, 105000);
+			count++;
+		} else {
+			dprintk(CVP_INFO, "%s: DMA Allocation success\n", __func__);
+			break;
+		}
+	}
 	if (IS_ERR_OR_NULL(kvaddr)) {
 		dprintk(CVP_ERR, "%s: failed dma allocation\n", __func__);
 		goto fail_dma_alloc;
@@ -2741,12 +2756,17 @@ static void __session_clean(struct cvp_hal_session *session)
 {
 	struct cvp_hal_session *temp, *next;
 	struct iris_hfi_device *device;
+	struct msm_cvp_core *core = NULL;
+	struct msm_cvp_inst *inst = NULL;
+	void *tmp = NULL;
 
 	if (!session || !session->device) {
 		dprintk(CVP_WARN, "%s: invalid params\n", __func__);
 		return;
 	}
 	device = session->device;
+	core = cvp_driver->cvp_core;
+	inst = (struct msm_cvp_inst *) session->session_id;
 	dprintk(CVP_SESS, "deleted the session: %pK\n", session);
 	/*
 	 * session might have been removed from the device list in
@@ -2758,6 +2778,13 @@ static void __session_clean(struct cvp_hal_session *session)
 			break;
 		}
 	}
+	/* Remove the IDR id assigned to this session */
+	mutex_lock(&core->idr_mtx);
+	tmp = idr_remove(&core->sess_idr, inst->sess_id);
+	if (tmp != session)
+		dprintk(CVP_WARN, "%s: session\n", __func__);
+	mutex_unlock(&core->idr_mtx);
+
 	/* Poison the session handle with zeros */
 	*session = (struct cvp_hal_session){ {0} };
 	kfree(session);
@@ -2825,6 +2852,9 @@ static int iris_hfi_session_init(void *device, void *session_id,
 	struct cvp_hfi_cmd_sys_session_init_packet pkt;
 	struct iris_hfi_device *dev;
 	struct cvp_hal_session *s;
+	struct msm_cvp_core *core;
+	struct msm_cvp_inst *inst;
+	int id = 0;
 
 	if (!device || !new_session) {
 		dprintk(CVP_ERR, "%s - invalid input\n", __func__);
@@ -2832,6 +2862,8 @@ static int iris_hfi_session_init(void *device, void *session_id,
 	}
 
 	dev = device;
+	core = cvp_driver->cvp_core;
+	inst = session_id;
 	mutex_lock(&dev->lock);
 
 	s = kzalloc(sizeof(*s), GFP_KERNEL);
@@ -2842,15 +2874,34 @@ static int iris_hfi_session_init(void *device, void *session_id,
 
 	s->session_id = session_id;
 	s->device = dev;
+
+	mutex_lock(&core->idr_mtx);
+	idr_preload(GFP_KERNEL);
+	/* Need to think if we can use core->lock or dev->lock or need a
+	 * different new lock for this?
+	 */
+	id = idr_alloc(&core->sess_idr, (void *)s, 0x7FFF0000, INT_MAX, GFP_NOWAIT);
+	idr_preload_end();
+	mutex_unlock(&core->idr_mtx);
+	if (id < 0) {
+		dprintk(CVP_ERR,
+			"%s: idr allocation failed for session %pK of inst %pK\n",
+			__func__, s, session_id);
+		goto err_session_init_fail;
+	}
+
 	dprintk(CVP_SESS,
-		"%s: inst %pK, session %pK\n", __func__, session_id, s);
+		"%s: inst %pK, session %pK, idr_id = 0x%x\n", __func__, session_id, s, id);
 
 	list_add_tail(&s->list, &dev->sess_head);
 
 	__set_default_sys_properties(device);
 
+	inst->sess_id = id;
+
 	if (call_hfi_pkt_op(dev, session_init, &pkt, s)) {
 		dprintk(CVP_ERR, "session_init: failed to create packet\n");
+		inst->sess_id = 0x0000DEAD;
 		goto err_session_init_fail;
 	}
 
@@ -2864,6 +2915,7 @@ static int iris_hfi_session_init(void *device, void *session_id,
 err_session_init_fail:
 	if (s)
 		__session_clean(s);
+	inst->sess_id = 0;
 	*new_session = NULL;
 	mutex_unlock(&dev->lock);
 	return -EINVAL;
@@ -2987,6 +3039,7 @@ static int iris_hfi_session_set_buffers(void *sess, u32 iova, u32 size)
 	u64 ktid;
 	struct msm_cvp_core *core;
 	struct msm_cvp_inst *inst = NULL;
+	u32 session_id = 0;
 
 	if (!session || !session->device || !iova || !size) {
 		dprintk(CVP_ERR, "Invalid Params\n");
@@ -3002,7 +3055,8 @@ static int iris_hfi_session_set_buffers(void *sess, u32 iova, u32 size)
 	}
 
 	core = cvp_driver->cvp_core;
-	inst = cvp_get_inst_from_id(core, hash32_ptr(session));
+	session_id = ((struct msm_cvp_inst *)(session->session_id))->sess_id;
+	inst = cvp_get_inst_from_id(core, session_id);
 	if (!inst) {
 		dprintk(CVP_ERR, "%s: invalid session\n", __func__);
 		rc = -EINVAL;
@@ -3039,6 +3093,7 @@ static int iris_hfi_session_release_buffers(void *sess)
 	u64 ktid;
 	struct msm_cvp_core *core;
 	struct msm_cvp_inst *inst = NULL;
+	u32 session_id = 0;
 
 	if (!session || !session->device) {
 		dprintk(CVP_ERR, "Invalid Params\n");
@@ -3054,7 +3109,8 @@ static int iris_hfi_session_release_buffers(void *sess)
 	}
 
 	core = cvp_driver->cvp_core;
-	inst = cvp_get_inst_from_id(core, hash32_ptr(session));
+	session_id = ((struct msm_cvp_inst *)(session->session_id))->sess_id;
+	inst = cvp_get_inst_from_id(core, session_id);
 	if (!inst) {
 		dprintk(CVP_ERR, "%s: invalid session\n", __func__);
 		rc = -EINVAL;
@@ -3499,9 +3555,11 @@ static struct cvp_hal_session *__get_session(struct iris_hfi_device *device,
 		u32 session_id)
 {
 	struct cvp_hal_session *temp = NULL;
+	struct msm_cvp_inst *inst = NULL;
 
 	list_for_each_entry(temp, &device->sess_head, list) {
-		if (session_id == hash32_ptr(temp))
+		inst = (struct msm_cvp_inst *)temp->session_id;
+		if (session_id == inst->sess_id)
 			return temp;
 	}
 
@@ -3641,6 +3699,7 @@ int __response_handler(struct iris_hfi_device *device)
 		/* Process the packet types that we're interested in */
 		process_system_msg(info, device, raw_packet);
 
+		/* This session_id is a double pointer to the idr_id of session */
 		session_id = get_session_id(info);
 		/*
 		 * hfi_process_msg_packet provides a session_id that's a hashed
@@ -3652,11 +3711,6 @@ int __response_handler(struct iris_hfi_device *device)
 		if (session_id) {
 			struct cvp_hal_session *session = NULL;
 
-			if (upper_32_bits((uintptr_t)*session_id) != 0) {
-				dprintk(CVP_ERR,
-					"Upper 32-bits != 0 for sess_id=%pK\n",
-					*session_id);
-			}
 			session = __get_session(device,
 					(u32)(uintptr_t)*session_id);
 			if (!session) {
