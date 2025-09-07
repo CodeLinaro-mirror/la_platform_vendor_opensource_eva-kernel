2fb0074cdfed3811872bc2cfd86cd600d4bdb4f8
Bhargavi Routhu <brouthu@qti.qualcomm.com>
2025-09-07 14:52:50 +0530

    msm: eva: Fixing KASAN UAF issue
    handle session timeout thread may access an already freed inst
    object in case of race condition with msm_cvp_destroy called
    by msm_cvp_close.
    We are not validating the inst pointer before trying to access
    core object from inst.
    
    ported from eva-kernel.lnx.4.0 to eva-kernel.lnx.1.1
    
    Change-Id: I12ee3198150be8c88b9a56b124f159b395f55f42

--- msm/eva/msm_cvp_buf.c
+++ msm/eva/msm_cvp_buf.c
@@ -1222,9 +1222,10 @@ int msm_cvp_register_buffer(struct msm_cvp_inst *inst,
 	struct cvp_hfi_device *hdev;
 	struct cvp_hal_session *session;
 	struct msm_cvp_inst *s;
+	struct msm_cvp_core *core = NULL;
 	int rc = 0;
 
-	if (!inst || !inst->core || !buf) {
+	if (!inst || !buf) {
 		dprintk(CVP_ERR, "%s: invalid params\n", __func__);
 		return -EINVAL;
 	}
@@ -1232,7 +1233,13 @@ int msm_cvp_register_buffer(struct msm_cvp_inst *inst,
 	if (!buf->index)
 		return 0;
 
-	s = cvp_get_inst_validate(inst->core, inst);
+	core = list_first_entry(&cvp_driver->cores, struct msm_cvp_core, list);
+	if (!core) {
+		dprintk(CVP_ERR, "%s: core is NULL", __func__);
+		return -EINVAL;
+	}
+
+	s = cvp_get_inst_validate(core, inst);
 	if (!s)
 		return -ECONNRESET;
 
@@ -1257,9 +1264,10 @@ int msm_cvp_unregister_buffer(struct msm_cvp_inst *inst,
 		struct eva_kmd_buffer *buf)
 {
 	struct msm_cvp_inst *s;
+	struct msm_cvp_core *core = NULL;
 	int rc = 0;
 
-	if (!inst || !inst->core || !buf) {
+	if (!inst || !buf) {
 		dprintk(CVP_ERR, "%s: invalid params\n", __func__);
 		return -EINVAL;
 	}
@@ -1267,7 +1275,13 @@ int msm_cvp_unregister_buffer(struct msm_cvp_inst *inst,
 	if (!buf->index)
 		return 0;
 
-	s = cvp_get_inst_validate(inst->core, inst);
+	core = list_first_entry(&cvp_driver->cores, struct msm_cvp_core, list);
+	if (!core) {
+		dprintk(CVP_ERR, "%s: core is NULL", __func__);
+		return -EINVAL;
+	}
+
+	s = cvp_get_inst_validate(core, inst);
 	if (!s)
 		return -ECONNRESET;
 
